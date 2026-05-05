f7ff90b4aa850bb6a8c7e18508ef6c14ba80e27b
Yonas Sahile <yonassahile8@gmail.com>
2026-05-05 20:49:17 +0300

    feat:models of my web digital camera

--- backend/camera/digital/models.py
+++ backend/camera/digital/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class CameraCapture(models.Model):
+    # 'captures/' is the subfolder inside your MEDIA_ROOT
+    image = models.ImageField(upload_to='captures/%Y/%m/%d/')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    label = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"Capture {self.id} - {self.timestamp}"
