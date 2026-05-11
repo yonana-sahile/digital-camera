a682567dea7088b117fe7f43465611242eb028a5
Yonas Sahile <yonassahile8@gmail.com>
2026-05-11 03:34:15 +0300

    feat:updated the serializers.py of webcamera

--- backend/camera/digital/serializers.py
+++ backend/camera/digital/serializers.py
@@ -1,7 +1,12 @@
+# pip install django-extra-fields
 from rest_framework import serializers
+from drf_extra_fields.fields import Base64ImageField
 from .models import CameraCapture
 
 class CameraCaptureSerializer(serializers.ModelSerializer):
-    class Meta:
+    # This automatically converts the Base64 string from React into a saved file
+    image = Base64ImageField(required=True)
+
+    class Model:
         model = CameraCapture
-        fields = ['id', 'image', 'timestamp', 'label']
+        fields = ['id', 'image', 'timestamp']
