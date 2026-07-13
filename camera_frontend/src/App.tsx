75e32054eca91a657ee6b2baaaacf022f2d5dbcd
Yonas Sahile <yonassahile8@gmail.com>
2026-07-13 18:18:57 +0300

    chore: updated component api endpoints to live render production link

--- camera_frontend/src/App.tsx
+++ camera_frontend/src/App.tsx
@@ -1,9 +1,9 @@
+import React from 'react';
 import CameraCapture from './components/CameraCapture';
 
 function App() {
   return (
     <div className="App">
-      <h1>Django + React Digital Camera</h1>
       <CameraCapture />
     </div>
   );
